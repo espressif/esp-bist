1e2760b2879e1038af7a950b11bc2d418b34b6fa
Lucas Tamborrino <lucas.tamborrino@espressif.com>
2025-05-21 08:47:05 +0200

    wdt: Fix watchdog test
    Update values to microseconds
    
    Signed-off-by: Lucas Tamborrino <lucas.tamborrino@espressif.com>

--- src/bist/core/wdt/bist_wdt.c
+++ src/bist/core/wdt/bist_wdt.c
@@ -26,7 +26,7 @@
 
 bist_esp_err_t bist_wdt_test(void)
 {
-    uint32_t wdt_timeout_ms = 100;
+    uint32_t wdt_timeout_us = 100;
     soc_reset_reason_t reset = esp_rom_get_reset_reason(0);
 
     if (reset == RESET_REASON_CORE_MWDT0) {
@@ -35,19 +35,10 @@ bist_esp_err_t bist_wdt_test(void)
 
     BIST_ADD_LABEL("bist_test_wdt_timeout");
 
-    wdt_init(wdt_timeout_ms);
+    wdt_init(wdt_timeout_us);
 
     /* let watchdog callback happen or timeout */
-    /* code below only works with real device */
-    // uint32_t now = esp_rtc_get_time_us() + 1000000;
-    // while (esp_rtc_get_time_us() < now) {
-    //     ets_delay_us(100000);
-    // }
-
-    /* let watchdog callback happen or timeout */
-    for (uint32_t k = 0; k < 1000; k++) {
-        ets_delay_us(1000);
-    }
+    ets_delay_us(1000);
 
     wdt_deinit();
 
