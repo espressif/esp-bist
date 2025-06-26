6f2a57dafe9fe874f2ccb9b79340faccb6c91e05
Lucas Tamborrino <lucas.tamborrino@espressif.com>
2025-06-26 12:15:39 -0300

    tests: Add digital IO test
    Signed-off-by: Lucas Tamborrino <lucas.tamborrino@espressif.com>

---
+++ tests/digital_io_test/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "bist_esp.h"
+#include "unity.h"
+#include "hal/gpio_ll.h"
+
+#define BIST_TEST_GPIO_EXT_OUT_IO               (2)
+#define BIST_TEST_GPIO_EXT_IN_IO                (3)
+#define BIST_TEST_GPIO_SIGNAL_IDX               (SIG_IN_FUNC97_IDX)
+
+void test_BIST_IO_INVALID_GPIO(void)
+{
+    bist_esp_err_t ret = bist_gpio_output_test(SOC_GPIO_PIN_COUNT);
+    TEST_ASSERT_EQUAL(BIST_ESP_IO_TEST_ERR, ret);
+    ret = bist_gpio_input_test(SOC_GPIO_PIN_COUNT, 0);
+    TEST_ASSERT_EQUAL(BIST_ESP_IO_TEST_ERR, ret);
+    ret = bist_gpio_output_test(-1);
+    TEST_ASSERT_EQUAL(BIST_ESP_IO_TEST_ERR, ret);
+    ret = bist_gpio_input_test(-1, 0);
+    TEST_ASSERT_EQUAL(BIST_ESP_IO_TEST_ERR, ret);
+}
+
+void test_BIST_IO_OUTPUT_GPIO(void)
+{
+    bist_esp_err_t ret = bist_gpio_output_test(BIST_TEST_GPIO_EXT_OUT_IO);
+    TEST_ASSERT_EQUAL(BIST_ESP_OK, ret);
+}
+
+int main()
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_BIST_IO_INVALID_GPIO);
+    RUN_TEST(test_BIST_IO_OUTPUT_GPIO);
+    return UNITY_END();
+}
