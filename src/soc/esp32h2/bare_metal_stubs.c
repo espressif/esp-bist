60ff15848930ca73e3576304e990b8473bc81f21
Lucas Tamborrino <lucas.tamborrino@espressif.com>
2026-03-09 14:36:18 -0300

    Add ESP32-H2 SoC support
    - Add src/soc/esp32h2/ port: start.c, vectors.S, clock_init.c,
      bare_metal_stubs.c, linker.ld, sdkconfig.h
    - Add cmake/esp32h2.cmake and esp32h2 branch in cmake/project.cmake
    - Add components/hal/efuse_hal.c override for SOC_ECDSA (declare
      efuse_hal_read for generic HAL when building for ESP32-H2)
    
    Signed-off-by: Lucas Tamborrino <lucas.tamborrino@espressif.com>

---
+++ src/soc/esp32h2/bare_metal_stubs.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024 Espressif Systems (Shanghai) Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Bare-metal stubs for symbols normally provided by ROM or optional IDF
+ * components. Allows linking without full IDF/ROM when running BIST only.
+ */
+
+#include <stdint.h>
+
+#include "esp_private/periph_ctrl.h"
+#include "esp_private/esp_modem_clock.h"
+
+__attribute__((weak)) void modem_clock_module_enable(periph_module_t module)
+{
+    (void)module;
+}
+
+__attribute__((weak)) void modem_clock_module_disable(periph_module_t module)
+{
+    (void)module;
+}
