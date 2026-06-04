4688facced927f8485f7891b07f0a1c13d371c12
Sylvio Alves <sylvio.alves@espressif.com>
2026-06-04 09:57:57 -0300

    soc: declare esp_sleep_pd_config stub as weak
    Other bare-metal stubs are weak so a real IDF impl can override them.
    This one was strong: if IDF's esp_sleep.c ever links, the linker errors
    with multiple definitions. Mark it weak in c5/c6/h2 ports.
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>

--- src/soc/esp32c6/clock_init.c
+++ src/soc/esp32c6/clock_init.c
@@ -29,6 +29,7 @@
 /* Must match IDF esp_sleep.h declaration to avoid conflicting types. */
 #include "esp_sleep.h"
 #include "esp_err.h"
+__attribute__((weak))
 esp_err_t esp_sleep_pd_config(esp_sleep_pd_domain_t domain, esp_sleep_pd_option_t option)
 {
     (void)domain;
